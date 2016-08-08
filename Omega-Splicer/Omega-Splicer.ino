6ab27734268e55289eaf171542c3bae4b5268a6c
TheoNaciri <theo.naciri@gmail.com>
2016-08-09 01:52:54 +0200

    Cleanup

--- Omega-Splicer/Omega-Splicer.ino
+++ Omega-Splicer/Omega-Splicer.ino
@@ -1,30 +1,28 @@
 /*
-The sketch receives a Bluetooth Low Energy 4 connection,
-then uses PWM on the RGB shield to approximate the color
-selected.
+This is the Arduino code for Omega-Splicer
+usage :
+  m1:int[;m2:int] -> controls motor 1 [and 2]
 
-This sketch is supposed to be used with the rfduinoColorWheel
-iPhone application.
+The module sends every SIGNAL_INTERVAL :
+  Approximate battery level in volts eg : "b:3.33" float
+  Approximate signal strength eg : "s:-51" int
+  Approximate temperature in Celsius by default eg : "t:25.50" float
+
+
+Sketch uses 28,212 bytes (21%) of program storage space. Maximum is 131,072 bytes.
+Global variables use 1,912 bytes of dynamic memory.
 */
 
 #include <RFduinoBLE.h>
 #include <stdlib.h>
 
 int led1 = 2;
-int led2 = 3;
-int led3 = 4;
-
 #define SIGNAL_INTERVAL 150 // in loops
-char ret[10];
 
 void setup() {
-  // setup the leds for output
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);  
-  pinMode(led3, OUTPUT);
-  Serial.begin(9600);
-  analogReference(VBG);
   RFduinoBLE.deviceName = "OmegaSplicer"; 
+  Serial.begin(9600);
+  analogReference(VBG); // for battery
 
   // this is the data we want to appear in the advertisement
   // (if the deviceName and advertisementData are too long to fix into the 31 byte
@@ -32,12 +30,11 @@ void setup() {
   // a single byte, then it will truncate the deviceName) max = 15
   // 'OmegaSplicer' + 'rgb' = 12 + 3 = 15
   RFduinoBLE.advertisementData = "cmd";
-send_temp(CELSIUS);
-send_battery();
   RFduinoBLE.advertisementInterval = 500; // in ms
-  //RFduinoBLE.customUUID = "58409710-D5E2-4A7D-B439-10CF9C59E89F";
+  send_temp(CELSIUS);
+  send_battery();
+  //RFduinoBLE.customUUID = "58409710-D5E2-4A7D-B439-10CF9C59E89F"; // comment for debug
   // start the BLE stack
-  Serial.println("Starting Omega-Splicer.");
   RFduinoBLE.begin();
 }
 
@@ -47,20 +44,29 @@ void loop() {
 }
 
 void RFduinoBLE_onConnect() {
-  // the default starting color on the iPhone is white
-  analogWrite(led1, 15);
-  analogWrite(led2, 125);
-  analogWrite(led3, 200);
+  // Led slightly lights up on connect
+  analogWrite(led1, 100);
 }
 
 void RFduinoBLE_onDisconnect() {
-  // turn all leds off on disconnect and stop pwm
+  // turn led off
   digitalWrite(led1, LOW);
-  digitalWrite(led2, LOW);
-  digitalWrite(led3, LOW);
 }
 
-// Send signal strength
+void RFduinoBLE_onReceive(char *data, int len) {
+  if (len > 3) {
+    if (!strncmp(data, "m1:", 3)) {
+      Serial.print("Pushing m1 to ");
+      Serial.println(data + 3);
+      if (char *m2 = strchr(data, ';')) {
+        Serial.print("Pushing m2 to ");
+        Serial.println(data + 3);
+      }
+    }
+  }
+}
+
+// Sends signal strength + temp + battery every SIGNAL_INTERVAL loops
 void RFduinoBLE_onRSSI(int rssi){
   static unsigned int total = 0;
   if (!(total % SIGNAL_INTERVAL)) {
@@ -89,31 +95,4 @@ void send_battery() {
   RFduinoBLE.sendFloat(b);
 }
 
-void RFduinoBLE_onReceive(char *data, int len) {
-  if (len > 3) {
-    if (!strncmp(data, "m1:", 3)) {
-      Serial.print("Pushing m1 to ");
-      Serial.println(data + 3);
-      if (char *m2 = strchr(data, ';')) {
-        Serial.print("Pushing m2 to ");
-        Serial.println(data + 3);
-      }
-    }
-  }
-}
 
-/*
-char *dtostrf (double val, signed char width, unsigned char prec, char *sout) {
-  char fmt[20];
-  sprintf(fmt, "%%%d.%df", width, prec);
-  sprintf(sout, fmt, val);
-  return sout;
-}*/
-
-/* get the RGB values
-    uint8_t r = data[0];
-    uint8_t g = data[1];
-
-    // set PWM for each led
-    analogWrite(led1, r);
-    analogWrite(led2, g); */
