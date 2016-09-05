d58ffcb4c03f5d16aeaed6b20f30d3575be9712f
Théo Naciri <theo.naciri@gmail.com>
2016-09-05 20:38:07 +0200

    Cleanup sent data

--- Omega-Splicer/Omega-Splicer.ino
+++ Omega-Splicer/Omega-Splicer.ino
@@ -1,12 +1,12 @@
 /*
 This is the Arduino code for Omega-Splicer
 usage :
-  m1:int[;m2:int] -> controls motor 1 [and 2]
+  m1:int[;m2:int] -> controls motor 1 [and 2] 0 to 255
 
-The module sends every SIGNAL_INTERVAL :
-  Approximate battery level in volts eg : "b:3.33" float
-  Approximate signal strength eg : "s:-51" int
-  Approximate temperature in Celsius by default eg : "t:25.50" float
+The module sends every SIGNAL_INTERVAL (defaults to (~ 8 seconds) :
+  Approximate battery level in percentage from 3 to 4.2V eg : "b:16" for 3.2V. In int format
+  Approximate signal strength eg : "s:-51" Value in int format
+  Approximate temperature in Celsius by default eg : "t:25" Value in int format
 
 
 Sketch uses 28,212 bytes (21%) of program storage space. Maximum is 131,072 bytes.
@@ -15,9 +15,12 @@ Global variables use 1,912 bytes of dynamic memory.
 
 #include <RFduinoBLE.h>
 #include <stdlib.h>
+#include <string>
 
 int led1 = 2;
-#define SIGNAL_INTERVAL 150 // in loops
+int led2 = 3;
+int led3 = 4;
+#define SIGNAL_INTERVAL 150 // in loops, 8 seconds
 
 void setup() {
   RFduinoBLE.deviceName = "OmegaSplicer"; 
@@ -28,12 +31,12 @@ void setup() {
   // (if the deviceName and advertisementData are too long to fix into the 31 byte
   // ble advertisement packet, then the advertisementData is truncated first down to
   // a single byte, then it will truncate the deviceName) max = 15
-  // 'OmegaSplicer' + 'rgb' = 12 + 3 = 15
+  // 'OmegaSplicer' + 'cmd' = 12 + 3 = 15
   RFduinoBLE.advertisementData = "cmd";
   RFduinoBLE.advertisementInterval = 500; // in ms
   send_temp(CELSIUS);
   send_battery();
-  //RFduinoBLE.customUUID = "58409710-D5E2-4A7D-B439-10CF9C59E89F"; // comment for debug
+  RFduinoBLE.customUUID = "58409710-D5E2-4A7D-B439-10CF9C59E89F"; // comment for debug
   // start the BLE stack
   RFduinoBLE.begin();
 }
@@ -54,45 +57,74 @@ void RFduinoBLE_onDisconnect() {
 }
 
 void RFduinoBLE_onReceive(char *data, int len) {
+  char *cmd;
+  Serial.print("->");
+  Serial.print(data);
+  Serial.println("<-");
   if (len > 3) {
-    if (!strncmp(data, "m1:", 3)) {
-      Serial.print("Pushing m1 to ");
-      Serial.println(data + 3);
-      if (char *m2 = strchr(data, ';')) {
-        Serial.print("Pushing m2 to ");
-        Serial.println(data + 3);
+    cmd = strtok(data, "/");
+    while (cmd != NULL) {
+      if (!strncmp(cmd, "m1:", 3)) { // refaire un parser
+       Serial.print("Pushing m1 to ");
+       Serial.println(cmd + 3);
+       analogWrite(led2, atoi(cmd + 3));
+      } else if (!strncmp(cmd, "m2:", 3)) {
+       Serial.print("Pushing m2 to ");
+       Serial.println(cmd + 3);
+       analogWrite(led3, atoi(cmd + 3));
       }
+      cmd = strtok(NULL, "/");
     }
   }
+  memset(data, 0, len);
 }
 
 // Sends signal strength + temp + battery every SIGNAL_INTERVAL loops
 void RFduinoBLE_onRSSI(int rssi){
   static unsigned int total = 0;
   if (!(total % SIGNAL_INTERVAL)) {
-    String sig = String("s:");
-    sig += rssi;
-    Serial.println(sig);
+    display_sig(rssi);
     send_temp(CELSIUS);
     send_battery();
+    Serial.println(" ");
   }
   total++;
 }
 
+void display_sig(int rssi) {
+  String sig = String("s:");
+  sig += rssi;
+  Serial.println(sig);
+}
+
 void send_temp(int scale) {
-  float tmp = RFduino_temperature(scale);
-  Serial.print("t:");
-  Serial.println(tmp);
-  RFduinoBLE.send("t:",2);
-  RFduinoBLE.sendFloat(tmp);
+  char buffer[6];
+  String ret = String("t:");
+
+  ret += String((int)(RFduino_temperature(scale)));
+  ret.toCharArray(buffer, 6);
+  
+  Serial.println(buffer);
+  RFduinoBLE.send(buffer, 6);
+//  RFduinoBLE.send(floatToString(buffer, tmp));
 }
 
 void send_battery() {
-  Serial.print("b:");
-  RFduinoBLE.send("b:", 2);
-  float b = analogRead(1)/1023.0*5.0;
-  Serial.println(b);
-  RFduinoBLE.sendFloat(b);
+  char buffer[6];
+  String ret = String("b:");
+  float minimum = 3.0;
+  float maximum = 4.2;
+  float exact_level = analogRead(1)/1023.0*5.0;
+  
+  int bat = ((int) (exact_level - minimum) / (maximum - minimum) * 100);
+  if (bat > 100)
+    bat = 100;
+  else if (bat < 0)
+    bat = 0;
+  ret += String(bat);
+  ret.toCharArray(buffer, 6);
+  
+  Serial.println(buffer);
+  RFduinoBLE.send(buffer, 6);
 }
-
 
